e52971567d99e8c64f9c79280c6b489a6deb06c2
Buddy Williams <buddywilliams@gmail.com>
2021-07-06 10:53:47 -0400

    wip: checkpoint, experimenting with Ant Design layouts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,65 @@
-import React from "react";
+import React from 'react';
+import './App.css';
 
-// We use Route in order to define the different routes of our application
-import { Route } from "react-router-dom";
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 
-// We import all the components we need in our app
-import Navbar from "./components/navbar";
-import Edit from "./components/edit";
-import Create from "./components/create";
-import RecordList from "./components/record-list";
+const { SubMenu } = Menu;
+const { Header, Content, Footer, Sider } = Layout;
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <Navbar />
-      <Route exact path="/">
-        <RecordList />
-      </Route>
-      <Route path="/edit/:id" component={Edit} />
-      <Route path="/create">
-        <Create />
-      </Route>
+    <div className="App" id="components-layout-demo-top-side">
+      <Layout>
+        <Header className="header">
+          <div className="logo">Future Money</div>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px', marginTop: '3.8em' }}>
+          {/* <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb> */}
+          <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+            <Sider className="site-layout-background" width={200}>
+              <Menu
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                style={{ height: '100%' }}
+              >
+                <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+                  <Menu.Item key="1">option1</Menu.Item>
+                  <Menu.Item key="2">option2</Menu.Item>
+                  <Menu.Item key="3">option3</Menu.Item>
+                  <Menu.Item key="4">option4</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+                  <Menu.Item key="5">option5</Menu.Item>
+                  <Menu.Item key="6">option6</Menu.Item>
+                  <Menu.Item key="7">option7</Menu.Item>
+                  <Menu.Item key="8">option8</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
+                  <Menu.Item key="9">option9</Menu.Item>
+                  <Menu.Item key="10">option10</Menu.Item>
+                  <Menu.Item key="11">option11</Menu.Item>
+                  <Menu.Item key="12">option12</Menu.Item>
+                </SubMenu>
+              </Menu>
+            </Sider>
+            <Content style={{ padding: '0 24px', minHeight: 'calc(100vh - 17em)' }}>Content</Content>
+          </Layout>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Future Money</Footer>
+      </Layout>
     </div>
   );
-};
+}
 
 export default App;
